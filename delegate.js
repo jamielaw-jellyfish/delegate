7eaa935d188892cba45c33ba6b6301618da9c560
Jamie Law <jamiemlaw@gmail.com>
2019-07-17 17:13:58 +0100

    Create delegate.js

---
+++ delegate.js
@@ -0,0 +1,16 @@
+function delegate(selector, fn) {
+  var fns = (typeof selector === 'string') ? { selector: fn } : selector;
+  
+  return function (e) {
+    var target, selector;
+
+    for (target = e.target; target && target !== e.currentTarget; target = target.parentNode) {
+      for (selector in fns) {
+        if (!fns.hasOwnProperty(selector)) continue;
+        if ((selector === 'default' && !called) || target.matches(selector)) {
+          fns[selector].call(target, e);
+        }
+      }
+    }
+  }
+}
